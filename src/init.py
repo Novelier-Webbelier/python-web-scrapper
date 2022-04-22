24dbb18b266025adc90d47964ac676b6183d071a
Novelier Webbelier <novelier.webbelier@gmail.com>
2022-04-22 23:34:10 +0900

    #2.2 Navigating with Python

--- src/init.py
+++ src/init.py
@@ -1,2 +1,7 @@
-print("Hello World")
+import requests
 
+indeed_url = 'https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&salary&radius=100&l&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=6a7446eb57ad8e77'
+
+indeed_result = requests.get(indeed_url)
+
+print(indeed_result.text)
